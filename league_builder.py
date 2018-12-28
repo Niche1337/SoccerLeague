c4d012c93cc0660eb279671124c5e8a3a2523e56
Nitesh Parmar <niteshp2007@gmail.com>
2018-12-28 19:52:37 +0000

    Update league_builder.py

--- league_builder.py
+++ league_builder.py
@@ -1,14 +1,13 @@
 import csv
 soccer_csv = "soccer_players.csv"
 
-if __name__ == "__main__":
-    file_reader(soccer_csv)
-
 
 def file_reader(soccer_file):
     with open(soccer_file, newline = '') as file:
-        art_reader = csv.DictReader(file, delimeter = ",")
+        art_reader = csv.DictReader(file)
         rows = list(art_reader)
         for row in rows:
             print(row)
 
+if __name__ == "__main__":
+    file_reader(soccer_csv)
