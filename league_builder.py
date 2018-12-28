c7131917c1fd1bf3e8d4a4db0372a80ccfc7321b
Nitesh Parmar <niteshp2007@gmail.com>
2018-12-28 19:48:03 +0000

    Update league_builder.py
    Started to created file reader

--- league_builder.py
+++ league_builder.py
@@ -1,4 +1,15 @@
+import csv
+soccer_csv = "soccer_players.csv"
+
 if __name__ == "__main__":
     pass
 
 
+def file_reader(soccer_file):
+    with open(soccerFile, newline = '') as file:
+        art_reader = csv.DictReader(file, delimeter = ",")
+        rows = list(art_reader)
+        for row in rows:
+            print(row)
+
+file_reader(soccer_csv)
