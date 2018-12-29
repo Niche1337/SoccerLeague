1fca72c8820928d76f52dfc804631f84a62dfa28
Nitesh Parmar <niteshp2007@gmail.com>
2018-12-29 17:11:53 +0000

    Added more logic

--- league_builder.py
+++ league_builder.py
@@ -3,11 +3,30 @@ soccer_csv = "soccer_players.csv"
 
 
 def file_reader(soccer_file):
+    rows = []
     with open(soccer_file, newline = '') as file:
         art_reader = csv.DictReader(file)
         rows = list(art_reader)
-        for row in rows:
-            print(row)
+        #
+    return rows
+
+def team_creator(players):
+    sharks = []
+    dragons = []
+    raptors = []
+
+    print(players[0]['Name'])
+
+
+
+def team_file_creator(team1, team2, team3):
+    with open("teams.txt", "a") as file:
+        file.writelines(("This is a test"))
+
 
 if __name__ == "__main__":
-    file_reader(soccer_csv)
+    players = file_reader(soccer_csv)
+    team_creator(players)
+
+##TODO write up team creator func
+## TODO write up team file writer 
