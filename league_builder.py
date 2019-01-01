590cf8873786387fa602b8526351419aa50ab89c
Nitesh Parmar <niteshp2007@gmail.com>
2019-01-01 12:00:38 +0000

    All text file seem to be correct

--- league_builder.py
+++ league_builder.py
@@ -49,28 +49,26 @@ def team_file_creator(team1, team2, team3):
         for name in team_names:
             file.write(("{}\n".format(name)))
             if name == "Sharks":
+                player_letter(team1, team_names[0])
                 for players in team1:
                     file.write("{}, {}, {}\n".format(players["Name"], players["Soccer Experience"], players["Guardian Name(s)"]))
                 file.write("-"*40 + "\n"*3)
             elif name == "Dragons":
+                player_letter(team2, team_names[1])
                 for players in team2:
                     file.write("{}, {}, {}\n".format(players["Name"], players["Soccer Experience"], players["Guardian Name(s)"]))
                 file.write("-"*40 + "\n"*3)
             elif name == "Raptors":
+                player_letter(team3,team_names[2])
                 for players in team3:
                     file.write("{}, {}, {}\n".format(players["Name"], players["Soccer Experience"], players["Guardian Name(s)"]))
                 file.write("-"*40 + "\n"*3)
 
-def player_letter(soccer_file):
-    players = []
-    with open(soccer_file, newline = '') as file:
-        art_reader = csv.DictReader(file)
-        players = list(art_reader)
-    for player in players:        
-        with open("{}.txt".format(player["Name"]), "w") as file:
-            file.write("Hello World")
-        
-
+def player_letter(soccer_file, team_name):
+    for player in soccer_file:        
+        player_name = player["Name"].split()
+        with open("{}_{}.txt".format(player_name[0], player_name[1]), "w") as file:        
+                file.write("Dear {}\n\t {} has been assigned to the team {},\n the date and time of the first practise is 02/02/2019, 09.00".format(player["Guardian Name(s)"], player["Name"], team_name))
 
 
 
@@ -78,5 +76,4 @@ if __name__ == "__main__":
     players = file_reader(SOCCER_CSV)
     sharks, dragons, raptors = team_creator(players)
     team_file_creator(sharks, dragons, raptors)
-    player_letter(SOCCER_CSV)
 
